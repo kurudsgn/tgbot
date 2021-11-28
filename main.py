59923a8735c213a6bf559cbc5c768284047c0f2e
kurudsgn <vladislav.podgornov326@yandex.ru>
2021-11-29 00:17:47 +0300

    v1

---
+++ main.py
@@ -0,0 +1,27 @@
+import telebot
+from telebot import types
+
+token = "2108949376:AAEPQGM8R2JAOOqJtjYCP1cG5bo_lmF6CNQ"
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Хочу", "/help")
+    bot.send_message(message.chat.id, 'Привет! Хочешь узнать свежую информацию о МТУСИ?', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я умею...')
+
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == "хочу":
+        bot.send_message(message.chat.id, 'Тогда тебе сюда – https://mtuci.ru/')
+
+
+bot.polling(non_stop=True)
